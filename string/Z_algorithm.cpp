e50b01bb43c9d876990d18d7279a6f06d8db7418
Suman Bera <118124036+bera474@users.noreply.github.com>
2025-01-03 11:56:25 +0530

    Update Z_algorithm.cpp

--- string/Z_algorithm.cpp
+++ string/Z_algorithm.cpp
@@ -1,13 +1,18 @@
 vector<int> z_function(string s) {
-    int n = (int) s.length();
+    int n = s.size();
     vector<int> z(n);
-    for (int i = 1, l = 0, r = 0; i < n; ++i) {
-        if (i <= r)
-            z[i] = min (r - i + 1, z[i - l]);
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
-            ++z[i];
-        if (i + z[i] - 1 > r)
-            l = i, r = i + z[i] - 1;
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
     }
     return z;
 }
