1d6c7052282645a65fd910dbf49cbc5dd1037b07
Suman Bera <118124036+bera474@users.noreply.github.com>
2025-01-10 21:21:37 +0530

    Update LongestPalindromicPrefix.cpp

--- string/LongestPalindromicPrefix.cpp
+++ string/LongestPalindromicPrefix.cpp
@@ -2,8 +2,7 @@ int LongestPalindromicPrefix(string s) {
   string t = s + '?';
   reverse(s.begin(), s.end());
   t += s; int n = t.length();
-  int lps[n];
-  fill(lps, lps + n, 0);
+  vector<int> lps(n, 0);
   for (int i = 1; i < n; i++) {
     int len = lps[i - 1];
     while (len > 0 && t[len] != t[i])
