3879844eba30b9f62079e19a0b06002281e5f74c
Suman Bera <118124036+bera474@users.noreply.github.com>
2025-01-21 18:09:57 +0530

    Update dsu.cpp

--- DSU/dsu.cpp
+++ DSU/dsu.cpp
@@ -1,10 +1,10 @@
 class DSU {
-  vector<int> f, siz, rnk;
+  vector<int> f, siz;
 
 public:
   DSU(int n) {
-    siz.assign(n, 1); rnk.assign(n, 0);
-    f.resize(n); iota(begin(f), end(f), 0);
+    f.resize(n); siz.assign(n, 1);
+    iota(f.begin(), f.end(), 0);
   }
 
   int find(int x) { return (x == f[x] ? x : f[x] = find(f[x])); }
@@ -13,8 +13,7 @@ public:
   bool merge(int x, int y) {
     x = find(x), y = find(y);
     if (x == y) return false;
-    if (rnk[x] < rnk[y]) swap(x, y);
-    rnk[x] += (rnk[x] == rnk[y]);
+    if (siz[x] < siz[y]) swap(x, y);
     f[y] = x; siz[x] += siz[y];
     return true;
   }
