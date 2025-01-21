9e34f83b86a07412ab40875cd573f9715304323a
Suman Bera <118124036+bera474@users.noreply.github.com>
2025-01-21 17:50:07 +0530

    Update dsu.cpp

--- DSU/dsu.cpp
+++ DSU/dsu.cpp
@@ -1,35 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
+class DSU {
+  vector<int> f, siz, rnk;
 
-struct DSU {
-  vector<int> f, siz;
-
-  DSU() {}
-  DSU(int n) { init(n); }
-
-  void init(int n) {
-    f.resize(n);
-    iota(f.begin(), f.end(), 0);
-    siz.assign(n, 1);
+public:
+  DSU(int n) {
+    siz.assign(n, 1); rnk.assign(n, 0);
+    f.resize(n); iota(begin(f), end(f), 0);
   }
 
-  int find(int x) {
-    while (x != f[x]) {
-      x = f[x] = f[f[x]];
-    }
-    return x;
-  }
-
+  int find(int x) { return (x == f[x] ? x : f[x] = find(f[x])); }
   bool same(int x, int y) { return find(x) == find(y); }
 
   bool merge(int x, int y) {
-    x = find(x);
-    y = find(y);
-    if (x == y) {
-      return false;
-    }
-    siz[x] += siz[y];
-    f[y] = x;
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (rnk[x] < rnk[y]) swap(x, y);
+    rnk[x] += (rnk[x] == rnk[y]);
+    f[y] = x; siz[x] += siz[y];
     return true;
   }
 
